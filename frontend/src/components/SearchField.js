32caf6717f6af1094f1d6989219da8063cc97169
Myron Lacey <mjlx42@gmail.com>
2021-12-05 23:12:14 -0500

    Put frontend in its own folder

---
+++ frontend/src/components/SearchField.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react';
+import * as styles from '../style/SearchField.module.css';
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
+
+const Suggestion = ({ suggestion, onClick }) => {
+    return (
+        <div className={styles.tag} onClick={onClick}>
+            {suggestion.label}
+            <div className={styles.line} />
+        </div>
+    );
+};
+
+function SearchField({ setLocation }) {
+    const [suggestions, setSuggestions] = useState([]);
+    const [focus, setFocus] = useState(false);
+    const [choose, setChoose] = useState(false);
+    const [value, setValue] = useState('');
+    useEffect(() => {
+        const exec = setTimeout(async () => {
+            const query = value.trim();
+            if (query) {
+                const provider = new OpenStreetMapProvider();
+                const results = await provider.search({ query });
+                setSuggestions(results);
+            } else {
+                setSuggestions([]);
+            }
+        }, 500);
+        return () => clearTimeout(exec);
+    }, [value]);
+    return (
+        <div className={styles.container}>
+            <input
+                type="text"
+                placeholder="Search for a location"
+                onChange={(e) => {
+                    setValue(e.target.value);
+                    setChoose(false);
+                }}
+                value={value}
+                onFocus={() => setFocus(true)}
+                onBlur={() => {
+                    setTimeout(() => {
+                        if (!choose && suggestions.length !== 0) {
+                            setFocus(false);
+                            setValue(suggestions[0].label);
+                            setLocation(suggestions[0].x, suggestions[0].y);
+                        }
+                    }, 100);
+                }}
+            />
+            <div className={styles.suggestion}>
+                {focus &&
+                    suggestions.map((suggestion) => (
+                        <Suggestion
+                            key={suggestion.x}
+                            suggestion={suggestion}
+                            onClick={() => {
+                                setValue(suggestion.label);
+                                setLocation(suggestion.x, suggestion.y);
+                                setChoose(true);
+                            }}
+                        />
+                    ))}
+            </div>
+        </div>
+    );
+}
+
+export default SearchField;
