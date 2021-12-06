6e678da70ae69b79dbb1e40b3ef3fb657f4e415c
Myron Lacey <mjlx42@gmail.com>
2021-12-06 13:37:07 -0500

    Fix CORS and update environment.yml

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,28 @@
 from logging import debug
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from algorithm import GenerateMap
 
 app = Flask(__name__)
+CORS(app)
+cors = CORS(app, resources={
+    r"/*": {
+        "origins": "*"
+    }
+})
 
 @app.route("/create_path", methods=['POST'])
 def create_path():
     data = request.json
     map_generator = GenerateMap()
     G = map_generator.create_graph(data["city_state"], "drive")
-    orig_node = map_generator.neareast_node(G, map_generator.address_to_coords(data["start"]))
-    dest_node = map_generator.neareast_node(G, map_generator.address_to_coords(data["end"]))
+    orig_node = map_generator.neareast_node(G, (data["start"]["x"], data["start"]["y"]))
+    dest_node = map_generator.neareast_node(G, (data["end"]["x"], data["end"]["y"]))
     path = []
     if data["mode"] == "max":
-        path = map_generator.between_algorithm(G, orig_node, dest_node, data["percent"])
+        path = map_generator.between_algorithm(G, orig_node, dest_node, float(data["extra_distance"]))
     else:
-        path = map_generator.dijkstra_algorithm(G, orig_node, dest_node, False, data["percent"])
+        path = map_generator.dijkstra_algorithm(G, orig_node, dest_node, False, float(data["extra_distance"]))
     response = {
         "path": map_generator.path_to_coords(G, path),
         "elevation": map_generator.path_elevation(G, path),
